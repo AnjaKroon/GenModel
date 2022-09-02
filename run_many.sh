9d773c5b904dccffd93ded4cf3d467fc51968cea
AnjaKroon <68068874+AnjaKroon@users.noreply.github.com>
2022-09-02 15:53:06 -0400

    put s_stat into txt file as output

--- run_many.sh
+++ run_many.sh
@@ -12,11 +12,12 @@ do
 for b in 30 50 100 
 do
 /usr/local/bin/python3 /Users/anja/Documents/Flo_Research/GenModel/discrete.py ${U} ${m} ${e} ${b} > time_${U}_${m}_${e}_${b}.txt
-/usr/local/bin/python3 /Users/anja/Documents/Flo_Research/GenModel/gen_S.py ${U} ${m} ${e} ${b}
+/usr/local/bin/python3 /Users/anja/Documents/Flo_Research/GenModel/gen_S.py ${U} ${m} ${e} ${b} > S_stat_${U}_${m}_${e}_${b}.txt
 
 mv ModProbDist.png ModProbDist_${U}_${m}_${e}_${b}.png # uncomment this for the big run
 mv Gen_Samples.npy Gen_Samples_${U}_${m}_${e}_${b}.npy
 cat time_*.txt >> time.txt
+cat S_stat_*.txt >> S_stats.txt
 mkdir Out_${U}_${m}_${e}_${b}
 mv Gen_Samples_${U}_${m}_${e}_${b}.npy ModProbDist_${U}_${m}_${e}_${b}.png time_${U}_${m}_${e}_${b}.txt histo_${U}_${m}_${e}_${b}.csv Out_${U}_${m}_${e}_${b}/
 
--- run_one.sh
+++ run_one.sh
@@ -4,9 +4,9 @@ e=10 # decimals cause issues in the file name. Actual value is this divided by 1
 b=100
 
 /usr/local/bin/python3 /Users/anja/Documents/Flo_Research/GenModel/discrete.py ${U} ${m} ${e} ${b} > time_${U}_${m}_${e}_${b}.txt
-/usr/local/bin/python3 /Users/anja/Documents/Flo_Research/GenModel/gen_S.py ${U} ${m} ${e} ${b}
+/usr/local/bin/python3 /Users/anja/Documents/Flo_Research/GenModel/gen_S.py ${U} ${m} ${e} ${b} > S_stat_${U}_${m}_${e}_${b}.txt
 
 mv ModProbDist.png ModProbDist_${U}_${m}_${e}_${b}.png 
 mv Gen_Samples.npy Gen_Samples_${U}_${m}_${e}_${b}.npy 
 mkdir Out_${U}_${m}_${e}_${b}
-mv Gen_Samples_${U}_${m}_${e}_${b}.npy ModProbDist_${U}_${m}_${e}_${b}.png time_${U}_${m}_${e}_${b}.txt histo_${U}_${m}_${e}_${b}.csv Out_${U}_${m}_${e}_${b}
+mv Gen_Samples_${U}_${m}_${e}_${b}.npy ModProbDist_${U}_${m}_${e}_${b}.png time_${U}_${m}_${e}_${b}.txt histo_${U}_${m}_${e}_${b}.csv S_stat_${U}_${m}_${e}_${b}.txt Out_${U}_${m}_${e}_${b}
