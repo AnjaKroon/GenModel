5c6a4cd8e75757118bcbe9817a4a19a9da830c41
AnjaKroon <68068874+AnjaKroon@users.noreply.github.com>
2022-09-02 11:30:01 -0400

    fixed an error in gen_S

--- run_one.sh
+++ run_one.sh
@@ -4,7 +4,7 @@ e=10 # decimals cause issues in the file name. Actual value is this divided by 1
 b=100
 
 /usr/local/bin/python3 /Users/anja/Documents/Flo_Research/GenModel/discrete.py ${U} ${m} ${e} ${b} > time_${U}_${m}_${e}_${b}.txt
-/usr/local/bin/python3 /Users/anja/Documents/Flo_Research/GenModel/gen_S.py
+/usr/local/bin/python3 /Users/anja/Documents/Flo_Research/GenModel/gen_S.py ${U} ${m} ${e} ${b} > histo_time_${U}_${m}_${e}_${b}.csv
 
 mv ModProbDist.png ModProbDist_${U}_${m}_${e}_${b}.png
 mv Gen_Samples.npy Gen_Samples_${U}_${m}_${e}_${b}.npy 
