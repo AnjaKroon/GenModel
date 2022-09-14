03107f282b881c3b7127d8b9ec425953e8d626c0
floregol <florence.robert-regol@mail.mcgill.ca>
2022-09-14 11:07:18 -0400

    added plot for accurate ranking from S stat.

--- flo_poisson.py
+++ flo_poisson.py
@@ -1,28 +1,50 @@
+import fractions
 from discrete import makeUniProbArr, errFunct, genValArr, sampleSpecificProbDist
 from gen_S import empirical_dist, genSstat
 from plot_utils import plot_S_stat
 from sampling.poisson import poisson_empirical_dist
 import sys
 import numpy as np
-import random 
+import random
+
+
+def get_S(trials, U, m, tempered, with_poisson=True):
+    uni_prob_arr = makeUniProbArr(U)
+    prob_array = uni_prob_arr
+    if tempered:
+        prob_array = errFunct(U, uni_prob_arr, e, b)
+    S_trials = []
+    for _ in range(trials):
+        new_samples = sampleSpecificProbDist(genValArr(U), prob_array, m)
+        if with_poisson:
+            p_emp = poisson_empirical_dist(
+                U, m, new_samples, lambda m: sampleSpecificProbDist(genValArr(U), prob_array, m))
+        else:
+            p_emp = empirical_dist(
+                U, m, sampleSpecificProbDist(genValArr(U), prob_array, m))
+        s_statistic = genSstat(p_emp, U)
+        S_trials.append(s_statistic)
+    return S_trials
+
 
 if __name__ == '__main__':
     # Set the random seed
     np.random.seed(32)
     random.seed(321)
 
-    testCase = 2 # should be 1 or 2 depending on whether you want to run the program standalone or with a .sh script
-    if testCase ==1:
-        if len(sys.argv) != 6 :
+    testCase = 2  # should be 1 or 2 depending on whether you want to run the program standalone or with a .sh script
+    if testCase == 1:
+        if len(sys.argv) != 6:
             print("Usage:", sys.argv[0], "U m e b t")
-            sys.exit() 
+            sys.exit()
         #U = int(sys.argv[1])
         m = int(sys.argv[2])
-        e = float(sys.argv[3])/100 # recall this value has been multiplied by 100 in sh script
+        # recall this value has been multiplied by 100 in sh script
+        e = float(sys.argv[3])/100
         b = int(sys.argv[4])
         t = int(sys.argv[5])
         trials = t
-    
+
     if testCase == 2:
         #U = 100
         m = 1000
@@ -34,51 +56,43 @@ if __name__ == '__main__':
     S_uni_poisson = []
     S_tempered = []
     S_tempered_poisson = []
-    list_U = [int((i+3)*10) for i in range(10)] # 30, 40, 50, 60, 70, 80, 90, 100, 110, 120
+    rank = []
+    rank_poisson = []
+    list_U = [10, 50, 100, 150, 200, 250, 300]
     for U in list_U:
-        uni_prob_arr = makeUniProbArr(U)
+
         # uniform
-        S_uni_trials = []
-        S_uni_poisson_trials = []
-        for _ in range(trials): 
-            new_samples = sampleSpecificProbDist(genValArr(U), uni_prob_arr, m)
-            p_emp_dependent = empirical_dist(
-                U, m, sampleSpecificProbDist(genValArr(U), uni_prob_arr, m))
-            s_statistic = genSstat(p_emp_dependent, U)
-            S_uni_trials.append(s_statistic)
-            p_emp_dependent = poisson_empirical_dist(
-                U, m, new_samples, lambda m: sampleSpecificProbDist(genValArr(U), uni_prob_arr, m))
-            s_statistic = genSstat(p_emp_dependent, U)
-            S_uni_poisson_trials.append(s_statistic)
-
-        S_uni.append(S_uni_trials)
-        S_uni_poisson.append(S_uni_poisson_trials)
+        S_uni_U = get_S(trials, U, m, tempered=False, with_poisson=False)
+        S_uni_poisson_U = get_S(
+            trials, U, m, tempered=False, with_poisson=True)
         # tempered
+        S_tempered_U = get_S(trials, U, m, tempered=True, with_poisson=False)
+        S_tempered_poisson_U = get_S(
+            trials, U, m, tempered=True, with_poisson=True)
 
-        S_tempered_trials = []
-        S_tempered_poisson_trials = []
-        updated_prob_arr = errFunct(U, uni_prob_arr, e, b)
-        for _ in range(trials):
-            new_samples = sampleSpecificProbDist(
-                genValArr(U), updated_prob_arr, m)
-            p_emp_dependent = empirical_dist(U, m, new_samples)
-            s_statistic = genSstat(p_emp_dependent, U)
-            S_tempered_trials.append(s_statistic)
-            p_emp_dependent = poisson_empirical_dist(
-                U, m, new_samples, lambda m: sampleSpecificProbDist(genValArr(U), updated_prob_arr, m))
-            s_statistic = genSstat(p_emp_dependent, U)
-            S_tempered_poisson_trials.append(s_statistic)
-        S_tempered.append(S_tempered_trials)
-        S_tempered_poisson.append(S_tempered_poisson_trials)
+        # count the number of time s of ground truth is smaller than tempered version
+        fraction_rank = np.mean([S_uni_U[i] < S_tempered_U[i]
+                                for i in range(trials)])
+        fraction_poisson_rank = np.mean(
+            [S_uni_poisson_U[i] < S_tempered_poisson_U[i] for i in range(trials)])
 
-
-    
-    lines = {'g.t.': S_uni,
-             'g.t. with poisson.': S_uni_poisson,
-             'e=0.1': S_tempered,
-             'e=0.1 with poisson.': S_tempered_poisson}
-
-    
-    plot_S_stat(x=list_U, dict_y=lines, title='m_' +
-                str(m) + '_e_'+str(e)+'_trials_'+str(trials)+'_S.pdf')
+        # uniform
+        S_uni.append(S_uni_U)
+        S_uni_poisson.append(S_uni_poisson_U)
+        # tempered
+        S_tempered.append(S_tempered_U)
+        S_tempered_poisson.append(S_tempered_poisson_U)
+        rank.append(fraction_rank)
+        rank_poisson.append(fraction_poisson_rank)
+    lines_S = {'g.t.': S_uni,
+               'g.t. with poisson.': S_uni_poisson,
+               'e=0.1': S_tempered,
+               'e=0.1 with poisson.': S_tempered_poisson}
+    lines_rank = {
+        'g.t. vs tempered': rank,
+        'g.t. vs tempered with poisson': rank_poisson}
+    plot_S_stat(x=list_U, dict_y=lines_S, title='m_' +
+                str(m) + '_e_'+str(e)+'_trials_'+str(trials)+'_S.pdf', xlabel='|U|', ylabel='S')
+    plot_S_stat(x=list_U, dict_y=lines_rank, title='m_' +
+                str(m) + '_e_'+str(e)+'_trials_'+str(trials)+'_ranking.pdf', xlabel='|U|', ylabel='\% of accurate ranking')
     # U m e b "S of uniform": NUMBER "S of uniform with poisson":NUMBER "S of tempered":NUMBER
--- plot_utils.py
+++ plot_utils.py
@@ -1,20 +1,20 @@
 import matplotlib.pyplot as plt
-from matplotlib import markers
 import numpy as np
 import matplotlib
-import os
 import matplotlib.colors as mcolors
-import math
-import random
 from scipy.stats import bootstrap
-matplotlib.rcParams['text.usetex'] = True
-font = {'family' : 'normal',
-        'weight' : 'bold',
-        'size'   : 17}
+# sketchy way of figuring out if latex is installed, might fail.
+# If it does, comment out
+from distutils.spawn import find_executable
+if find_executable('latex'):
+    matplotlib.rcParams['text.usetex'] = True
+
+font = {'family': 'normal',
+        'weight': 'bold',
+        'size': 17}
 
 matplotlib.rc('font', **font)
 cycle = list(mcolors.TABLEAU_COLORS.values())
-random.shuffle(cycle)
 
 
 def get_color(i):
@@ -23,23 +23,29 @@ def get_color(i):
     else:
         return cycle[i]
 
+
 def get_ci(trials):
-    ci = bootstrap((trials,), np.mean, confidence_level=0.9,random_state=0)
+    ci = bootstrap((trials,), np.mean, confidence_level=0.9, random_state=0)
     return ci.confidence_interval
 
 
-def plot_S_stat(x, dict_y, title):
+def plot_S_stat(x, dict_y, title, xlabel, ylabel):
 
-    for i, (key, all_trials) in enumerate(dict_y.items()):
+    for i, (key, val) in enumerate(dict_y.items()):
         color = get_color(i)
-        y = [np.mean(trials) for trials in all_trials]
-        all_ci = [get_ci(trials) for trials in all_trials] 
-        plt.plot(x, y, color=color, label=key)
-        ci_over = [ci.high for i, ci in enumerate(all_ci)]
-        ci_under = [ci.low for i, ci in enumerate(all_ci)]
-        plt.fill_between(x, ci_under, ci_over, color=color, alpha=.1)
+        if type(val[0]) is list:
+            all_trials = val
+            y = [np.mean(trials) for trials in all_trials]
+            all_ci = [get_ci(trials) for trials in all_trials]
+            plt.plot(x, y, color=color, label=key)
+            ci_over = [ci.high for i, ci in enumerate(all_ci)]
+            ci_under = [ci.low for i, ci in enumerate(all_ci)]
+            plt.fill_between(x, ci_under, ci_over, color=color, alpha=.1)
+        else:
+            plt.plot(x, val, color=color, label=key)
     plt.legend()
-    plt.xlabel('$|U|$')
-    plt.ylabel('$S$')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
     plt.tight_layout()
     plt.savefig(title)
+    plt.close()
