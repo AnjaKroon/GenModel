caaf5bb83a4a99e9953396b354ea4061b2711aff
floregol <florence.robert-regol@mail.mcgill.ca>
2022-10-08 16:07:30 -0400

    stair function

--- stair.py
+++ stair.py
@@ -6,6 +6,7 @@ from scipy.stats import rv_discrete
 import math
 from time import time
 
+
 def stair(U, posU, ratio, S):
     # From my understanding
     # Take U * posU to get the amount of U that will have a stair function
@@ -13,44 +14,54 @@ def stair(U, posU, ratio, S):
     # ratio is highest pmf/lowest pmf -- representative of the amount of "y step" in between each stair
     # pmf of each value for the whole U will have to sum to 1
     # highest pmf / lowest pmf = ratio -- (?/common denom) / (?/common denom)
-    U_with_stair = U*posU
-    U_for_each_S = U_with_stair/S # be careful to consider the case this may be fractional
+    U_with_stair = int(U*posU/100)
+    U_zero = U - U_with_stair
 
-    middle_step = math.floor(S/2)
-    dist_middle_step = 1/U_with_stair
+    # be careful to consider the case this may be fractional
+    U_for_each_S = math.floor(U_with_stair/S)
+    U_for_last_S = U_with_stair - (S-1)*U_for_each_S
+    U_per_stairs = [U_for_each_S for i in range(S-1)]
+    U_per_stairs.append(U_for_last_S)
+
+    ratio_all_steps = list(np.arange(1, ratio, (ratio-1)/(S-1)))
+    ratio_all_steps.append(ratio)
+    p_first_floor = 1 / \
+        (np.sum([U_per_stairs[i]*ratio_step for i,
+         ratio_step in enumerate(ratio_all_steps)]))
+    p_each_stair = [p_first_floor *
+                    ratio_stair for ratio_stair in ratio_all_steps]
+    verify_that_is_one = np.sum(
+        [p_each_stair[i]*U_per_stairs[i] for i in range(S)])
+
+    U_per_stairs.reverse()
+    p_each_stair.reverse()
+    current_dist = 0
+    current_step = 0
     stair_histo = {}
-
-    # DIST_DELTA is the standardized value in which the pmf increments between each step
-    highest = ratio
-    lowest = 1
-    DIST_DELTA = ((highest-lowest)/(S-1))/U_with_stair
-
-    current_step = middle_step
-    current_dist = dist_middle_step
-    while (current_step>-1):
-        stair_histo[current_step] =  current_dist
-        current_dist = current_dist + DIST_DELTA
-        current_step = current_step - 1 # to iterate though the while loop/the left half of the steps
-    
-    current_step = middle_step # reset the value of current step
-    current_dist = dist_middle_step # reset the value of the current distribution
-    current_step = current_step +1 # I don't want to overwrite the middle step. I want to start at the next one
-    while (current_step<S):
-        current_dist = current_dist - DIST_DELTA
-        stair_histo[current_step] = current_dist
-        current_step = current_step +1 # to iterature through the while loop/the right half of the steps
-    
-    # to check
-
-
+    if U < 6**6:
+        for i, size_stair in enumerate(U_per_stairs):
+            current_dist = p_each_stair[i]
+            for _ in range(size_stair):
+                current_dist = p_each_stair[i]
+                stair_histo[current_step] = current_dist
+                current_step += 1
+    else:
+        start_interval = 0
+        for i, size_stair in enumerate(U_per_stairs):
+            current_dist = p_each_stair[i]
+            interval = [start_interval, start_interval+size_stair]
+            stair_histo[i] = {'interval': interval, 'p': current_dist}
+            start_interval += size_stair
     return stair_histo
 
 
 if __name__ == '__main__':
-    U = 6
-    posU = 10 # pos U is % of U with > 0 pmf
-    ratio = 3 # highest pmf/lowest pmf
-    S = 3
+    U = 78
+    posU = 23  # pos U is % of U with > 0 pmf
+    ratio = 3  # highest pmf/lowest pmf
+    S = 6
+
     # U posU ratio and S are parameters that will define the stair function
     stair_histo = stair(U, posU, ratio, S)
-    print (stair_histo)
+    print(np.sum(list(stair_histo.values())))
+    print(stair_histo)
