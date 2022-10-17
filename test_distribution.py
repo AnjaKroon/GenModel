3125ae59c200b986773b520b2989697b912dbd68
floregol <florence.robert-regol@mail.mcgil.ca>
2022-10-17 12:35:59 -0400

    cleaned up, added figures path

--- test_distribution.py
+++ test_distribution.py
@@ -1,13 +1,13 @@
+from asyncio import start_unix_server
 import math
+import os
 from tqdm import tqdm
 from discrete import makeUniProbArr, prob_array_to_dict
 from stair import make_stair_prob
 from statistic.generate_statistics import get_ranking_results, perform_binning_and_compute_stats, genSstat, generate_samples_scalable
 from plot_utils import plot_stat, put_on_plot
-import scipy
 import numpy as np
 import random
-
 
 
 if __name__ == '__main__':
@@ -18,113 +18,108 @@ if __name__ == '__main__':
     init_e = 0.1
     init_b = 30
     trials = 50
+
     S = 3
     ratio = 2
     distribution_type = 'STAIRS'  # STAIRS
+
     Bs = [4, 5, 6, 7, 8]
-    power_base = 6
+    power_base = 5
     list_U = [power_base**power_base]
     list_M = [1000]
-    # for m in list_M:
+
+    list_of_binning_algo = ['algo', 'random', 'none']
+    list_of_espilon_q = [0, init_e, init_e*1.5, init_e*2]
+    list_of_title_q = [
+        'no temper (uniform)', 'slightly tempered', 'medium tempered', 'heavily tempered']
+
+    # create list of results,
+    list_of_results_stats = []
+    for _ in list_of_binning_algo:
+        list_of_q_results_stats = []
+        for _ in list_of_espilon_q:
+            list_of_q_results_stats.append([])
+        list_of_results_stats.append(list_of_q_results_stats)
+
     for m in list_M:
         print("for this round m is ", m)
         for U in list_U:
             print("and U is ", U)
 
-            stat_uni = []
-            stat_temper = []
-            stat_mid_temper = []
-            stat_easy_temper = []
-            stat_uni_baseline = []
-            stat_temper_baseline = []
-            stat_mid_temper_baseline = []
-            stat_easy_temper_baseline = []
-            all_U = []
             if distribution_type == 'UNIFORM':
                 ground_truth_p = prob_array_to_dict(makeUniProbArr(U))
-                # put_on_plot(all_U, ground_truth_p)
+
             elif distribution_type == 'STAIRS':
                 ground_truth_p = make_stair_prob(
                     U, posU=(math.factorial(power_base)/U), ratio=ratio,  S=S)
-                for i in range(U+1):
-                    all_U.append(i)
-                #put_on_plot(all_U, ground_truth_p)
+
             else:
                 raise NotImplemented
-           # plot_stat('PMF_Uniform_Stairs.pdf', 'U', 'Probability')
-            # first, we generate all the samples here. The same samples should be reused for each B.
-            # If it takes too much memory, we can put this in a for loop.
-            ground_truth_samples_list = generate_samples_scalable(ground_truth_p,
-                                                                  trials, U, m, tempered=False, e=0, b=100)
-            tempered_samples_list = generate_samples_scalable(ground_truth_p,
-                                                              trials, U, m, tempered=True, e=init_e, b=init_b)
-            mid_tempered_samples_list = generate_samples_scalable(ground_truth_p,
-                                                                  trials, U, m, tempered=True, e=init_e*1.5, b=init_b)
-            easy_tempered_samples_list = generate_samples_scalable(ground_truth_p,
-                                                                   trials, U, m, tempered=True, e=init_e*2, b=init_b)
 
+            # obtain the samples
+            list_of_samples = []
+            for e in list_of_espilon_q:
+                if e == 0:
+                    samples = generate_samples_scalable(
+                        ground_truth_p, trials, U, m, tempered=False, e=0, b=100)
+                else:
+                    samples = generate_samples_scalable(
+                        ground_truth_p, trials, U, m, tempered=True, e=e, b=init_b)
+                list_of_samples.append(samples)
 
-            
             for B in tqdm(Bs):  # For each bin granularity
 
-                # this fonction takes the samples, compile statistics, then store the result in all_stat_list
-                def compile_all_stats(all_samples_list, all_stat_list, baseline_all_stat_list,   U,  B, title):
+                for i, all_samples_list in enumerate(list_of_samples):
 
                     S_trials_for_this_B_list = perform_binning_and_compute_stats(
                         all_samples_list, ground_truth_p, U, B, stat_func=genSstat)
                     algo_stat = [i['B_algo'] for i in S_trials_for_this_B_list]
                     random_stat = [i['B_random']
                                    for i in S_trials_for_this_B_list]
-                    all_stat_list.append(algo_stat)
-                    baseline_all_stat_list.append(random_stat)
-                    name = ("compile_stats_" + str(B) + str(title) + ".txt")
-                    with open(name, "w") as output:
-                        # writing baseline_all_stat_list into file -- should it be all_stat_list?
-                        output.write(str(baseline_all_stat_list))
 
-                # compile stats for ground truth
-                compile_all_stats(ground_truth_samples_list,
-                                  stat_uni, stat_uni_baseline, U, B=B, title="ground_truth_samples")
-                # compile stats for tempered
-                compile_all_stats(tempered_samples_list,
-                                  stat_temper, stat_temper_baseline, U, B=B, title="tempered_samples")
-                compile_all_stats(mid_tempered_samples_list, stat_mid_temper,
-                                  stat_mid_temper_baseline, U, B=B, title="mid_tempered_samples")  # compile stats for tempered
-                compile_all_stats(easy_tempered_samples_list, stat_easy_temper,
-                                  stat_easy_temper_baseline, U, B=B, title="easy_tempered_samples")  # compile stats for tempered
-
-
-                
-
+                    list_of_results_stats[0][i].append(algo_stat)
+                    list_of_results_stats[1][i].append(random_stat)
 
             print('Generating S plots...')
 
-            put_on_plot(Bs, {'no temper (uniform)': stat_uni, 'slightly tempered': stat_temper,
-                        'medium tempered': stat_mid_temper, 'heavily tempered': stat_easy_temper})
-
-            plot_stat('algo_S.pdf', 'Bins',
+            plotting_dict_algo = {}
+            for i, title in enumerate(list_of_title_q):
+                plotting_dict_algo[title] = list_of_results_stats[0][i]
+            put_on_plot(Bs, plotting_dict_algo)
+            prefix_title = 'U_' + str(U) + '_m_' + str(m)
+            prefix_title = os.path.join('figures', prefix_title)
+            plot_stat(prefix_title+'_algo_S.pdf', 'Bins',
                       'Empirical Total Variation Error')
 
-            put_on_plot(Bs, {'no temper (uniform)': stat_uni_baseline, 'slightly tempered': stat_temper_baseline,
-                         'medium tempered': stat_mid_temper_baseline, 'heavily tempered': stat_easy_temper_baseline})
-                         # error of _____ w.r.t ground truth
-                         # for no temper, samples are generated from uniform dist
-                         # for heavily temper, samples are generated from heavily tempered distribution
-                         # thus, the gen model should have an easier time distinguishing the heavily 
-                         # tempered case and will easily give it a lower rank
+            plotting_dict_random = {}
+            for i, title in enumerate(list_of_title_q):
+                plotting_dict_random[title] = list_of_results_stats[1][i]
+            put_on_plot(Bs, plotting_dict_random)
 
-            plot_stat('random_S.pdf', 'Bins',
+            # error of _____ w.r.t ground truth
+            # for no temper, samples are generated from uniform dist
+            # for heavily temper, samples are generated from heavily tempered distribution
+            # thus, the gen model should have an easier time distinguishing the heavily
+            # tempered case and will easily give it a lower rank
+
+            plot_stat(prefix_title+'_random_S.pdf', 'Bins',
                       'Empirical total variation error')
 
             print('Generating ranking plots...')
-            
-            algo_ranking_results_all_trials_all_Bs = [get_ranking_results(
-                [stat_uni[i], stat_temper[i], stat_mid_temper[i], stat_easy_temper[i]]) for i in range(len(Bs))]
 
-            random_ranking_results_all_trials_all_Bs = [get_ranking_results(
-                [stat_uni_baseline[i], stat_temper_baseline[i], stat_mid_temper_baseline[i], stat_easy_temper_baseline[i]]) for i in range(len(Bs))]
-            #ranking_results_mean = [np.mean(ranking_results_all_trials) for ranking_results_all_trials in ranking_results_all_trials_all_Bs]
+            algo_ranking_results_all_trials_all_Bs = []
+            for i in range(len(Bs)):
+                list_at_B = [q[i] for q in list_of_results_stats[0]]
+                algo_ranking_results_all_trials_all_Bs.append(
+                    get_ranking_results(list_at_B))
+
+            random_ranking_results_all_trials_all_Bs = []
+            for i in range(len(Bs)):
+                list_at_B = [q[i] for q in list_of_results_stats[1]]
+                random_ranking_results_all_trials_all_Bs.append(
+                    get_ranking_results(list_at_B))
 
             put_on_plot(Bs, {'algo': algo_ranking_results_all_trials_all_Bs,
                         'random': random_ranking_results_all_trials_all_Bs})
-            plot_stat('ranking.pdf', 'Bins','Kendall tau distance')
+            plot_stat(prefix_title + '_ranking.pdf',
+                      'Bins', 'Kendall tau distance')
